f03a8a85c5abbac253319dfdb31f9dca5129f180
Xdoxgg <m.zhuk007@gmail.com>
2025-12-26 21:19:23 +0300

    fix bug with ice dragonsteel armor

--- tags.js
+++ tags.js
@@ -9,11 +9,11 @@ ServerEvents.tags('item', event => {
     event.add('forge:dragonsteelbody', 'iceandfire:dragonsteel_lightning_chestplate')
     // LEGS
     event.add('forge:dragonsteellegs', 'iceandfire:dragonsteel_fire_leggings')
-    event.add('forge:dragonsteellegs', 'iceandfire:iceandfire:dragonsteel_ice_leggings')
+    event.add('forge:dragonsteellegs', 'iceandfire:dragonsteel_ice_leggings')
     event.add('forge:dragonsteellegs', 'iceandfire:dragonsteel_lightning_leggings')
     // BOOTS
     event.add('forge:dragonsteelboots', 'iceandfire:dragonsteel_fire_boots')
-    event.add('forge:dragonsteelboots', 'iceandfire:iceandfire:dragonsteel_ice_boots')
+    event.add('forge:dragonsteelboots', 'iceandfire:dragonsteel_ice_boots')
     event.add('forge:dragonsteelboots', 'iceandfire:dragonsteel_lightning_boots')
 
 })
