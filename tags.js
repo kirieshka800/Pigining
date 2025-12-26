a53486b7aef40263d7678612b20542284337b1a5
Xdoxgg <m.zhuk007@gmail.com>
2025-12-26 15:35:55 +0300

    add ise and fire armory tegs

---
+++ tags.js
@@ -0,0 +1,19 @@
+ServerEvents.tags('item', event => {
+    // HEADS
+    event.add('forge:dragonsteelhead', 'iceandfire:dragonsteel_fire_helmet')
+    event.add('forge:dragonsteelhead', 'iceandfire:dragonsteel_ice_helmet')
+    event.add('forge:dragonsteelhead', 'iceandfire:dragonsteel_lightning_helmet')
+    // BODY
+    event.add('forge:dragonsteelbody', 'iceandfire:dragonsteel_fire_chestplate')
+    event.add('forge:dragonsteelbody', 'iceandfire:dragonsteel_ice_chestplate')
+    event.add('forge:dragonsteelbody', 'iceandfire:dragonsteel_lightning_chestplate')
+    // LEGS
+    event.add('forge:dragonsteellegs', 'iceandfire:dragonsteel_fire_leggings')
+    event.add('forge:dragonsteellegs', 'iceandfire:iceandfire:dragonsteel_ice_leggings')
+    event.add('forge:dragonsteellegs', 'iceandfire:dragonsteel_lightning_leggings')
+    // BOOTS
+    event.add('forge:dragonsteelboots', 'iceandfire:dragonsteel_fire_boots')
+    event.add('forge:dragonsteelboots', 'iceandfire:iceandfire:dragonsteel_ice_boots')
+    event.add('forge:dragonsteelboots', 'iceandfire:dragonsteel_lightning_boots')
+
+})
